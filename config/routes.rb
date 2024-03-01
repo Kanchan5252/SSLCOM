d5a2cf02c1d5f6b5e4bcf48d0e19cba4b3c61a71
kanchan <kanchankamboj1020@gmail.com>
2024-03-01 11:36:34 +0530

    Authentication pages and header

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get '/dashboard', to: 'dashboard#index'
+  devise_scope :user do
+    authenticated :user do
+      root to: 'dashboard#index', as: :authenticated_root
+    end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+    unauthenticated :user do
+      root to: 'devise/registrations#new', as: :unauthenticated_root
+    end
+  end
 end
